2bc93fd53ab3fe98cba8e37902664c78a2a1726e
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2025-05-06 22:59:32 +0530

    Time: 4 ms (5.44%) | Memory: 20.8 MB (28.67%) - LeetSync

---
+++ 2048-build-array-from-permutation/build-array-from-permutation.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int>res;
+        for(int i=0;i<nums.size();i++){
+            res.emplace_back(nums[nums[i]]);
+        }
+        return res;
+    }
+};
