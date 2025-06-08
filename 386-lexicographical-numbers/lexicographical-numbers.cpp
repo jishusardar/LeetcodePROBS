f9d73ef0944cca63e0aa62aec19a70575bc79109
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2025-06-09 02:40:59 +0530

    Time: 75 ms (14.15%) | Memory: 28.1 MB (9.75%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -3,14 +3,12 @@ public:
     vector<int> lexicalOrder(int n) {
         vector<string>ans;
         for(int i=1;i<=n;i++){
-            string num=to_string(i);
-            ans.push_back(num);
+            ans.push_back(to_string(i));
         }
         sort(ans.begin(),ans.end());
         vector<int>fans;
         for(int i=0;i<ans.size();i++){
-            int number=stoi(ans[i]);
-            fans.push_back(number);
+            fans.push_back(stoi(ans[i]));
         }
         return fans;
     }
