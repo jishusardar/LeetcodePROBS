44324104637a13643628821b34693acf74f7738f
Jishu Sardar <73737848+jishusardar@users.noreply.github.com>
2025-07-01 12:53:35 +0530

    Time: 2 ms (48.46%) | Memory: 8.8 MB (60.22%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        vector<int>count(26,0);
         int ans=1;
         for(int i=0;i<word.size()-1;i++){
             if(word[i]==word[i+1]){
