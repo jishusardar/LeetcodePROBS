580a40927aefa9958ea37acaba41b79d763a3b74
Jishu Sardar <jishusardar01@gmail.com>
2025-08-19 22:26:28 +0530

    Time: 72 ms (77.01%) | Memory: 37.5 MB (67.27%) - LeetSync

--- 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        int num;
-        int maxi=INT_MIN;
-        int min;
-        int i,j;
+        int maximum=INT_MIN;
+        int minimum;
+        pair<int,pair<int,int>>p;
         vector<pair<int,pair<int,int>>>temp;
-        pair<int,pair<int,int>>Ele;
-        vector<int>ans(2);
-        int n=nums.size();
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.size();i++){
             temp.push_back(make_pair(nums[i][0],make_pair(i,0)));
-            maxi=max(maxi,nums[i][0]);
+            maximum=max(maximum,nums[i][0]);
         }
         priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>q(temp.begin(),temp.end());
-        Ele=q.top();
-        ans[0]=Ele.first;
-        ans[1]=maxi;
+        vector<int>ans(2);
+        ans[0]=q.top().first;
+        ans[1]=maximum;
+        pair<int,pair<int,int>>Ele;
+        int num,i,j;
         while(q.size()==nums.size()){
             Ele=q.top();
             q.pop();
@@ -25,11 +23,11 @@ public:
             j=Ele.second.second;
             if(j+1<nums[i].size()){
                 q.push(make_pair(nums[i][j+1],make_pair(i,j+1)));
-                maxi=max(maxi,nums[i][j+1]);
-                min=q.top().first;
-                if(maxi-min<ans[1]-ans[0]){
-                    ans[0]=min;
-                    ans[1]=maxi;
+                maximum=max(maximum,nums[i][j+1]);
+                minimum=q.top().first;
+                if(maximum-minimum<ans[1]-ans[0]){
+                    ans[0]=minimum;
+                    ans[1]=maximum;
                 }
             }
         }
