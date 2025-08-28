b5fc09c169fd860d1793363a1760b9a79dc05659
Jishu Sardar <jishusardar01@gmail.com>
2025-08-29 02:54:31 +0530

    Time: 73 ms (78.59%) | Memory: 56.1 MB (72.41%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -3,17 +3,15 @@ public:
      int atleast(vector<int>&nums,int k)
      {
         int start=0,end=0;
-        int count=0,size=0;
+        int count=0;
         unordered_map<int,int>mm;
         while(end<nums.size()){
             mm[nums[end]]++;
-            if(mm[nums[end]]==1)
-            size++;
-            while(size==k){
+            while(mm.size()==k){
                 count+=nums.size()-end;
                 mm[nums[start]]--;
                 if(mm[nums[start]]==0)
-                size--;
+                mm.erase(nums[start]);
                 start++;
             }
             end++;
