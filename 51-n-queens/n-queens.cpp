0d82c4f978875c839dac2e634c8f762fcedc1315
Jishu Sardar <jishusardar01@gmail.com>
2025-12-26 19:00:52 +0530

    Time: 2 ms (73.73%) | Memory: 10.1 MB (92.18%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-     bool check(int n,int i,int j,vector<string>&Board)
-     {
-        int row=i,colm=j;
-        while(row>-1&&colm>-1){
-            if(Board[row][colm]=='Q')
-            return 0;
-            row--,colm--;
-        }
-        row=i,colm=j;
-        while(row>-1&&colm<n){
-            if(Board[row][colm]=='Q')
-            return 0;
-            row--,colm++;
-        }
-        return 1;
-     }
-     void place(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&colm)
+     void place(int row,int n,vector<vector<string>>&ans,vector<string>&Board,vector<bool>&colm,vector<bool>&Leftdig,vector<bool>&Rightdig)
      {
         if(row==n){
             ans.push_back(Board);
             return;
         }
         for(int j=0;j<n;j++){
-            if(!colm[j]&&check(n,row,j,Board)){
+            if(!colm[j]&&!Rightdig[j+row]&&!Leftdig[(n-1)+j-row]){
                 colm[j]=1;
+                Leftdig[(n-1)+j-row]=1;
+                Rightdig[j+row]=1;
                 Board[row][j]='Q';
-                place(row+1,n,ans,Board,colm);
+                place(row+1,n,ans,Board,colm,Leftdig,Rightdig);
                 colm[j]=0;
                 Board[row][j]='.';
+                Leftdig[(n-1)+j-row]=0;
+                Rightdig[j+row]=0;
             }
         }
      }
@@ -36,11 +24,13 @@ public:
         vector<vector<string>>ans;
         vector<string>Board(n);
         vector<bool>colm(n);
+        vector<bool>Leftdig(2*n-1,0);
+        vector<bool>Rightdig(2*n-1,0);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++)
             Board[i].push_back('.');
         }
-        place(0,n,ans,Board,colm);
+        place(0,n,ans,Board,colm,Leftdig,Rightdig);
         return ans;
     }
 };
