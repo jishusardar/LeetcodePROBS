1bdc4771fcbb57b890825029990058f805546ada
Jishu Sardar <jishusardar01@gmail.com>
2026-02-08 15:56:36 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.50%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+    int find(int index,vector<int>&nums,int n,vector<int>&dp)
+    {
+        if(index>=n)
+        return 0;
+        if(dp[index]!=-1)
+        return dp[index];
+        return dp[index]=max(nums[index]+find(index+2,nums,n,dp),find(index+1,nums,n,dp));
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int pre=0,last=0,curr=0;
-        for(int i=n-1;i>=0;i--){
-            curr=max(nums[i]+last,pre);
-            last=pre;
-            pre=curr;
-        }
-        return curr;
+        vector<int>dp(n+2,-1);
+        return find(0,nums,n,dp);
     }
 };
