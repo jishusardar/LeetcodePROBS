96b23236df0aca9e5ffec54a1c95ebf958c6ec68
Jishu Sardar <jishusardar01@gmail.com>
2026-02-14 02:20:08 +0530

    Time: 31 ms (47.15%) | Memory: 36.6 MB (46.98%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    int find(int n,int amount,vector<int>&coins,vector<vector<int>>&dp){
-        if(amount==0)
-        return 1;
-        if(n<0)
-        return 0;
-
-        if(dp[n][amount]!=-1)
-        return dp[n][amount];
-
-        if(coins[n]>amount)
-        return dp[n][amount]=find(n-1,amount,coins,dp);
-        else
-        return dp[n][amount]=find(n,amount-coins[n],coins,dp)+find(n-1,amount,coins,dp);
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return find(n-1,amount,coins,dp);
+        vector<vector<unsigned int>>dp(n+1,vector<unsigned int>(amount+1,0));
+        for(int i=0;i<=n;i++)
+        dp[i][0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]>j)
+                dp[i][j]=dp[i-1][j];
+                else
+                dp[i][j]=dp[i][j-coins[i-1]]+dp[i-1][j];
+            }
+        }
+        return dp[n][amount];
     }
 };
