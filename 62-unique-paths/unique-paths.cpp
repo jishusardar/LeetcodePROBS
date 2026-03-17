fda5522580198ec26363be3ddda831957c900a1f
Jishu Sardar <jishusardar01@gmail.com>
2026-03-17 19:44:14 +0530

    Time: 3 ms (9.27%) | Memory: 9.5 MB (28.69%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>grid(m,vector<int>(n,1));
+        for(int i=m-2;i>=0;i--){
+            for(int j=n-2;j>=0;j--){
+                grid[i][j]=max(grid[i][j],grid[i][j+1]+grid[i+1][j]);
+            }
+        }
+        return grid[0][0];
+    }
+};
