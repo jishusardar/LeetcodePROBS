20ce20c21a6844ca78b6ca3c24744167b29161dc
Jishu Sardar <jishusardar01@gmail.com>
2026-04-08 18:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (58.03%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        int start,end,size=0,index;
-        vector<int>temp(n);
-        temp[0]=nums[0];
+        int start,end,mid,size=0,index;
+        vector<int>lis(n);
+        lis[0]=nums[0];
         for(int i=1;i<n;i++){
             start=0,end=size,index=size+1;
             while(start<=end){
-                int mid=start+(end-start)/2;
-                if(temp[mid]==nums[i]){
+                mid=start+(end-start)/2;
+                if(lis[mid]<nums[i])
+                start=mid+1;
+                else if(lis[mid]==nums[i]){
                     index=mid;
                     break;
                 }
-                else if(temp[mid]<nums[i])
-                start=mid+1;
                 else{
                     index=mid;
                     end=mid-1;
                 }
             }
-            temp[index]=nums[i];
+            lis[index]=nums[i];
             size=max(size,index);
         }
         return size+1;
